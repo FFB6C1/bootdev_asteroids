5a415b63a4a2f8561fac15e9ded88f809631e133
chesca <bubblebang@gmail.com>
2024-09-16 18:29:12 +0100

    player added, can move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,13 +8,27 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0), rect = None, special_flags= 0)
-        pygame.display.flip()
         dt = clock.tick(60)/1000
+
+        for item in updatable:
+            item.update(dt)
+        
+        for item in drawable:
+            item.draw(screen)
+
+        
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
